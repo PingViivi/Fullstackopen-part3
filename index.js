d12a8c6dc50b17eacaa5cf24ed1a09d23bd02ba5
Viivi Tuomi <viivi.tuomi@louhosdigital.fi>
2023-10-27 15:37:17 +0300

    port changed and cors installed

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const http = require('http')
 const express = require('express')
 var morgan = require('morgan')
-
+const cors = require('cors')
 const app = express()
 
 app.use(morgan('tiny'))
-
 app.use(express.json())
+app.use(cors())
 
+/*
 morgan.token('host', function(req, res) {
     console.log(res)
     return req.hostname;
 });
 app.use(morgan(':method :host :status :res[content-length] - :response-time ms'))
+*/
 
 let persons = [
     {
@@ -109,6 +111,7 @@ app.post('/api/persons', (request, response) => {
 
     response.json(person)
 })
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
